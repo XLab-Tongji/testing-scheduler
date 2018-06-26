4bfad853d28a983007859fe8401c9f2cdf163552
Leo Qi <qbzhhxx1996@qq.com>
2018-06-26 17:01:08 +0800

    修改代理地址

--- src/Global.vue
+++ src/Global.vue
@@ -1,6 +1,6 @@
 <script>
 const SERVER_ADDR = 'http://10.60.38.181:5310/'
-const AGENT_ADDR = 'http://localhost:5312/'
+const AGENT_ADDR = 'http://10.60.38.181:5312/'
 const CONDUCTOR_ADDR = 'http://10.60.38.181:5000/'
 export default {
 	SERVER_ADDR,
