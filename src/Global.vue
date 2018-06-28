6415b566a73bf8f234bbfa21d270a2028973ce48
Leo Qi <qbzhhxx1996@qq.com>
2018-06-28 11:46:41 +0800

    动态设置后台接口的地址（与UI服务器地址保持一致）

--- src/Global.vue
+++ src/Global.vue
@@ -1,9 +1,12 @@
 <script>
-const SERVER_ADDR = 'http://10.60.38.181:5310/'
-const AGENT_ADDR = 'http://10.60.38.181:5312/'
-const CONDUCTOR_ADDR = 'http://10.60.38.181:5000/'
+var BASE_URL_PREFIX = 'http://' + window.location.hostname
+var SERVER_ADDR = BASE_URL_PREFIX + ':' + 5310 + '/'
+var AGENT_ADDR = BASE_URL_PREFIX + ':' + 5312 + '/'
+var CONDUCTOR_ADDR = 'http://10.60.38.181:5000/'
+var SELF_ADDR = window.location.href
 export default {
 	SERVER_ADDR,
-	AGENT_ADDR
+	AGENT_ADDR,
+	SELF_ADDR
 }
 </script>
