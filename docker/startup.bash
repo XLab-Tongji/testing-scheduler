e6fbc7a03e2b7b1d527cd4b0780ba8bda7020e1a
Leo Qi <qbzhhxx1996@qq.com>
2018-07-02 19:11:36 +0800

    删除多余的临时测试脚本 修改生成的docker镜像名称和容器名称

--- docker/startup.bash
+++ docker/startup.bash
@@ -1,11 +1,11 @@
 # build server docker image
 conductor_network='docker_default'
 
-sudo docker build -t tmp/server -f ./server/Dockerfile  ../
-sudo docker run -d -p 5310:5310 --name tmp-server tmp/server
-sudo docker network connect docker_default tmp-server
+sudo docker build -t leoc/testing-scheduler:server -f ./server/Dockerfile  ../
+sudo docker run -d -p 5310:5310 --name testing-scheduler-server tmp/server
+sudo docker network connect docker_default testing-scheduler-server
 
 # build ui docker image
-sudo docker build -t tmp/ui -f ./ui/Dockerfile ../
-sudo docker run -d -p 5311:5311 -p 5312:5312 --name tmp-ui tmp/ui
-sudo docker network connect docker_default tmp-ui
+sudo docker build -t leoc/testing-scheduler:ui -f ./ui/Dockerfile ../
+sudo docker run -d -p 5311:5311 -p 5312:5312 --name testing-scheduler-ui tmp/ui
+sudo docker network connect docker_default testing-scheduler-server
