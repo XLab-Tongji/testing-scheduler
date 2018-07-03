be5e742e9cfd8b01d93419bcbd74d06804db3039
Leo Qi <qbzhhxx1996@qq.com>
2018-07-03 11:48:00 +0800

    恢复server原地址

--- ui/src/Global.vue
+++ ui/src/Global.vue
@@ -1,5 +1,6 @@
 <script>
-var SERVER_ADDR = 't-scheduler-server:5310/'
+var BASE_URL_PREFIX = 'http://' + window.location.hostname
+var SERVER_ADDR = BASE_URL_PREFIX + ':' + 5310 + '/'
 export default {
 	SERVER_ADDR
 }
