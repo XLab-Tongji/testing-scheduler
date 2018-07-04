955282a5e89553c2b07221e763284d3aed70a77d
Leo Qi <qbzhhxx1996@qq.com>
2018-07-04 19:41:19 +0800

    优化docker构建过程

--- docker/plugin/build.sh
+++ docker/plugin/build.sh
@@ -1,15 +1,15 @@
 basepath=$(cd `dirname $0`; pwd)					# get the absolute path of this shell file.
 tmppath=$basepath/tmp_files
-docker_tmppath=/root/build_dir
+docker_tmppath=/home/noroot/build_dir/conductor
 
 #build the gradle project in a temp container.
 mkdir -p $tmppath
 docker build -t leoc/conductor -f $basepath/Dockerfile $basepath
-docker run -d --name conductor-builder leoc/condcutor tail -f /dev/null
+docker run -d --name conductor-builder leoc/condcutor
 docker cp  conductor-builder:$docker_tmppath $tmppath/
 docker rm -f conductor-builder
 #docker rmi leoc/conductor
 
 #build the images of conductor.
-cd $tmppath/docker
+cd $tmppath/conductor/docker
 docker-compose build
--- docker/plugin/run.sh
+++ docker/plugin/run.sh
@@ -1,2 +1,4 @@
-
+basepath=$(cd `dirname $0`; pwd)					# get the absolute path of this shell file.
+tmppath=$basepath/tmp_files
+cd $tmppath/conductor/docker
 docker-compose up -d
