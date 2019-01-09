8446e84920011013b8b0604f0aeb33f720e2ec7b
1023578360@qq.com <1023578360@qq.com>
2019-01-09 14:33:22 +0800

    答辩

--- docker/clean.sh
+++ docker/clean.sh
@@ -6,3 +6,10 @@ sudo docker rm -f t-scheduler-server \
                   conductor_elasticsearch_1
 
 sudo docker network rm conductor_default
+
+basepath=$(cd `dirname $0`; pwd)					# get the absolute path of this shell file.
+
+cd plugin
+sudo rm -r tmp_files
+
+echo "--- Clean Finished ---"
