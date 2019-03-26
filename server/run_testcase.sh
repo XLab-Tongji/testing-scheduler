a632539f3694e104b2f19d8a320b24d4c33b7e9a
齐旭晨 <1023578360@qq.com>
2019-03-26 15:21:09 +0800

    Add the [sh]file to run testcase with command line

---
+++ server/run_testcase.sh
@@ -0,0 +1,7 @@
+# get the absolute path of this shell file.
+basepath=$(cd'dirname $0'; pwd)
+
+#run the testcase_target with command line
+sudo python $basepath/src/test_parser.py --filepath=$basepath/test/test_case/logic/tc_logic_01.yaml
+
+
