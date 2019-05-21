5276cb09e7762fd8a4a8a6d5de20abc39e3ebf1a
齐旭晨 <1023578360@qq.com>
2019-05-21 18:22:29 +0800

    5.21

--- server/run_testcase.sh
+++ server/run_testcase.sh
@@ -2,10 +2,10 @@
 basepath=$(cd'dirname $0'; pwd)
 
 #run the testcase_target with command line
-sudo python $basepath/src/test_parser.py --filepath=$basepath/test/test_case/stress/tc_stress_01.yaml
+#sudo python $basepath/src/test_parser.py --filepath=$basepath/test/test_case/stress/tc_stress_01.yaml
 
-sudo python $basepath/src/test_parser.py --filepath=$basepath/test/test_case/stress/tc_stress_02.yaml
+#sudo python $basepath/src/test_parser.py --filepath=$basepath/test/test_case/stress/tc_stress_02.yaml
 
-sudo python $basepath/src/test_parser.py --filepath=$basepath/test/test_case/stress/tc_stress_03.yaml
+#sudo python $basepath/src/test_parser.py --filepath=$basepath/test/test_case/stress/tc_stress_03.yaml
 
-
+sudo python /home/serveradmin/testing-scheduler/server/src/test_parser.py --filepath=/home/serveradmin/testing-scheduler/server/test/test_case/stress/stress.yaml
