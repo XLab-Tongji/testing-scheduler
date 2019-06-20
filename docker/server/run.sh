48c24cf4a34196fb284ad7a56af7b09f8294fc46
齐旭晨 <1023578360@qq.com>
2019-06-20 15:07:10 +0800

    run

--- docker/server/run.sh
+++ docker/server/run.sh
@@ -3,5 +3,5 @@ group="x-lab"
 server_image="$group/testing-scheduler:server"		# server image name
 server_container="t-scheduler-server"				# server container name
 
-#docker run -d --rm -p 5310:5310 -p 5312:5312 -p 5313:5313 -v /var/lib/testing-scheduler/server/test:/home/testing-scheduler/server/test/ -v /var/lib/testing-scheduler/server/src/env:/home/testing-scheduler/server/src/env --net=$conductor_network --name $server_container $server_image
-docker run -d --rm -p 5310:5310 -p 5312:5312 -p 5313:5313  --net=$conductor_network --name $server_container $server_image
+docker run -d --rm -p 5310:5310 -p 5312:5312 -p 5313:5313 -v /var/lib/testing-scheduler/server/test:/home/testing-scheduler/server/test/ -v /var/lib/testing-scheduler/server/src/env:/home/testing-scheduler/server/src/env --net=$conductor_network --name $server_container $server_image
+#docker run -d --rm -p 5310:5310 -p 5312:5312 -p 5313:5313  --net=$conductor_network --name $server_container $server_image
